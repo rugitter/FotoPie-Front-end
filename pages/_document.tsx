262b9d0f02145ca0a69a2c21c83c5c02f86526ae
Adamyyyy <happyadam2008@gmail.com>
2023-02-15 17:50:55 +1100

    build up home page and login page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import theme, { roboto } from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import theme, { roboto } from "../src/utils/theme";
+import createEmotionCache from "../src/utils/createEmotionCache";
 
 export default class MyDocument extends Document {
   render() {
@@ -69,7 +69,7 @@ MyDocument.getInitialProps = async (ctx) => {
   const emotionStyles = extractCriticalToChunks(initialProps.html);
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
       key={style.key}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: style.css }}
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,21 @@
+
+import Typography from "@mui/material/Typography";
+import MuiLink from "@mui/material/Link";
+
+export default function Copyright(props:any) {
+  return (
+<Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <MuiLink color="inherit" href="https://mui.com/">
+        FotoPie
+      </MuiLink>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
