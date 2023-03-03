d1c8c8c77da1d37ff2c4eaaf10b8e7b1d32398d4
Yingxin cai <cai19980122@gmail.com>
2023-03-03 18:40:58 +1100

    changes

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,13 +1,8 @@
 import { StrictMode } from "react";
 import React, { useState } from "react";
-
-import { useDropzone } from 'react-dropzone';
-import Grid from "@mui/material/Grid";
-import 'react-dropzone-uploader/dist/styles.css'
-import Dropzone from 'react-dropzone-uploader'
 import { Helmet } from "react-helmet"
 import Uploader from "../src/components/upload/uploader";
-// import SingleFileAutoSubmit  from "../src/components/upload/uploadImage";
+
 
 export default function MyDropzone() {
 
--- src/components/upload/uploader.tsx
+++ src/components/upload/uploader.tsx
@@ -1,80 +1,165 @@
 import 'react-dropzone-uploader/dist/styles.css'
-import Dropzone from 'react-dropzone-uploader'
-import { useState} from "react";
-// import Dropzone from 'react-dropzone'
+import Dropzone, { IFileWithMeta, StatusValue } from 'react-dropzone-uploader';
+import { useState } from "react";
+
+
 import axios, { AxiosRequestConfig, Method } from "axios";
-import { string } from 'yup';
 
 
-// import { DropzoneArea } from "material-ui-dropzone";
 
-export default function Uploader() { 
+export default function Uploader() {
   const [status, setStatus] = useState([]);
-  
 
-  const API_ENDPOINT = "https://882yhgxvdh.execute-api.ap-southeast-2.amazonaws.com/getPresignedImageURL"
-    
+  const API_ENDPOINT = "https://882yhgxvdh.execute-api.ap-southeast-2.amazonaws.com/getPresignedImageURL";
 
-  interface FileMeta {
-    name: string;
-    size: number;
-    type: string;
-  }
-  
-  interface DropzoneProps {
-    meta: FileMeta;
-  }
-  
-  const handleChangeStatus = (props: DropzoneProps, status: any): void => {
-    const { meta} = props;
+  const handleChangeStatus = async (
+    file: IFileWithMeta,
+    status: any,
+  ): Promise<void|{ meta: { [name: string]: any } }> => {
+    const { meta } = file;
     console.log(status, meta);
     setStatus(status);
+
+    if (status === "done") {
+      try {
+        const f = file.file;
+        console.log(f);
+
+        // Get request:presignedURL
+        const response = await axios({
+          method: "GET",
+          url: API_ENDPOINT,
+        });
+
+        console.log("Response", response);
+
+        // #put request:upload file to S3
+        const result = await fetch(response.data.uploadURL, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "image/jpeg",
+          },
+          body: f,
+        });
+
+        console.log("Result:", result.url);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
+
+  return (
+    <div className="uploader">
+    <Dropzone
+      onChangeStatus={handleChangeStatus}
+      maxFiles={1}
+      multiple={false}
+      canCancel={false}
+      inputContent= "Drop A Picture"
+      accept="image/*"
+
+      // styles={{
+      //   dropzone: { width: 400, height: 200,},
+      //   dropzoneActive: { borderColor: "green" },
+      // }}
+      />
+       </div>
+  );
+}
+
+//   const handleChangeStatus = async (file: IFileWithMeta,status: any, allFiles: IFileWithMeta[]): Promise<void | { meta: { [name: string]: any } }> => {
+//     const { meta, files} = file;
+//     console.log(status, meta, files);
+//     setStatus(status);
   
-  const handleSubmit = async (files: any) => {
-    const f = files[0]
-    console.log(f[files]);
+//     if (status === "done") {
+//       try {
+//         // const files = props.files;
+//         const f = files[0];
+//         console.log(f);
+        
+//         // Get request:presignedURL
+        
+//         const response = await axios({
+//           method: 'GET',
+//           url: API_ENDPOINT
+//         });
+        
+//         console.log('Response', response);
+            
+//         // #put request:upload file to S3
+//         const result = await fetch(response.data.uploadURL, {
+//           method: 'PUT',
+//           headers: {
+//             "Content-Type": "image/jpeg"
+//           },
+//           body: f
+//         });
+        
+//         console.log('Result:', result.url);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+//   };
+  
 
-    // Get request:presignedURL
+  
+    
+//       return (
+//         <Dropzone
+//           onChangeStatus={handleChangeStatus}
+//           // onSubmit={handleSubmit}
+//           maxFiles={1}
+//           multiple={false}
+//           canCancel={false}
+//           inputContent="Drop A File"
+//           // accept="image/*"
+//           styles={{
+//             dropzone: { width: 400, height: 200 },
+//             dropzoneActive: { borderColor: 'green' },
+//             }}
+//           />
+     
+//   )
+ 
+// };
+    
+//////
 
-    const response = await axios({
-      method: 'Get',
-      url: API_ENDPOINT
 
-    });
-    console.log('Response', response)
+  // const handleChangeStatus = (props: DropzoneProps, status: any): void => {
+  //   const { meta} = props;
+  //   console.log(status, meta);
+  //   setStatus(status);
+
+ 
+    
+  // };
+  
+  // const handleSubmit = async (files: any) => {
+  //   const f = files[0]
+  //   console.log(f[files]);
+
+  //   // Get request:presignedURL
+
+  //   const response = await axios({
+  //     method: 'Get',
+  //     url: API_ENDPOINT
+
+  //   });
+  //   console.log('Response', response)
     
 
-    // #put request:upload file to S3
-    const result = await fetch(response.data.uploadURL, {
-      method: 'PUT',
-      headers: {
-        "Content-Type":"image/jpeg"
-      },
-      body:f['file']
-    })
-    console.log('Result:', result.url)}
+  //   // #put request:upload file to S3
+  //   const result = await fetch(response.data.uploadURL, {
+  //     method: 'PUT',
+  //     headers: {
+  //       "Content-Type":"image/jpeg"
+  //     },
+  //     body:f['file']
+  //   })
+  //   console.log('Result:', result.url)}
 
         
-  
-    
-      return (
-        <Dropzone
-          onChangeStatus={handleChangeStatus}
-          onSubmit={handleSubmit}
-            maxFiles={1}
-            multiple={false}
-            canCancel={false}
-          inputContent="Drop A File"
-          accept="image/*"
-            styles={{
-              dropzone: { width: 400, height: 200 },
-              dropzoneActive: { borderColor: 'green' },
-            }}
-          />
-     
-  )
- 
-};
-    
-////////
