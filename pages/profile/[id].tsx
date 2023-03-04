aa60224ae6585110016c25ddd812bd75197bfb52
Adamyyyy <happyadam2008@gmail.com>
2023-03-04 11:39:33 +1100

    WI-17: style mui

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,8 +1,9 @@
-import { Container } from "@mui/material";
-import Image from "next/image";
+import { Button, Container, Grid } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axiosRequest from "../../src/utils/axiosRequest";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
 
 export default function ProfilePage() {
   const [lastName, setLastName] = useState("");
@@ -14,9 +15,9 @@ export default function ProfilePage() {
 
   useEffect(() => {
     if (!router.isReady) return;
-    
+
     axiosRequest(`/api/user/${id}`, "GET").then((res) => {
-      console.log(res);
+      if (id !== res.data.id) return router.push("/404");
       setLastName(res.data.lastName);
       setFirstName(res.data.firstName);
       setAvatar(res.data.avatar);
@@ -24,20 +25,38 @@ export default function ProfilePage() {
   }, [id]);
 
   return (
-    <Container maxWidth="lg">
-      <h1>Profile Page</h1>
-      <div>
-        {/* <Image
-          src={`${avatar}`}
-          alt="avatar"
-          width={200}
-          height={200}
-        /> */}
-      </div>
-      <div>
-        <p>{lastName}</p>
-        <p>{firstName}</p>
-      </div>
-    </Container>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      sx={{ mt: 10, width: "100%" }}
+    >
+      <Grid item>
+        <Avatar alt="avatar" src={avatar} sx={{ width: 180, height: 180 }} />
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        sx={{ mt: 5 }}
+        spacing={2}
+      >
+        <Grid item>
+          <Typography variant="h3">{firstName}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h3">{lastName}</Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container sx={{ mt: 5, ml: 10 }} spacing={5}>
+        <Grid item>
+          <Button variant="contained" sx={{borderRadius:10 }}>Gallery</Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained">Gallery</Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
