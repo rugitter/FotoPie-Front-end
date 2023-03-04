d9a03c291a0146f10dfef8560af971fd4a6aa280
Jeremy Liu <unswmercury@gmail.com>
2023-03-04 15:17:26 +1100

    final commit

--- pages/category/category-main-page.tsx
+++ pages/category/category-main-page.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import * as path from "path";
-import Image from "next/image";
 import PhotoCategory from "../../src/components/Category/PhotoCategory";
 import CategoryHeader from "../../src/components/Category/CategoryHeader";
 import Copyright from "../../src/components/Copyright";
