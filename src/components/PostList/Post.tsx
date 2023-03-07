66b95d7fe4f2bd847d22b52a0633dcb1f04beeb4
Taiyi Su <TaiyiSu98@gmail.com>
2023-03-08 10:36:37 +1100

    update

--- src/components/PostList/Post.tsx
+++ src/components/PostList/Post.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Link from "../../utils/Link";
 
 const Post = ({ url }: { url: string }) => {
   return (
+    //<Link href={}>
     <img
       src={url}
-      // srcSet={`${url}?w=100%&fit=crop&auto=format&dpr=2 2x`}
+      //srcSet={`${url}?w=100%&fit=crop&auto=format&dpr=2 2x`}
       alt=""
       loading="lazy"
+      //width={"100%"}
     />
+    //</Link>
   );
 };
 
