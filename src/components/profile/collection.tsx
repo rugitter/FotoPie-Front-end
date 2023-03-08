32224d92eb9fe46bda91cf5c1ae7749c6f3f19bf
Edward Chen <kunhong03@gmail.com>
2023-03-09 02:26:17 +1100

    fetch backend data through prop

--- src/components/profile/collection.tsx
+++ src/components/profile/collection.tsx
@@ -1,9 +1,33 @@
+import { useRouter } from "next/router";
+import axiosRequest from "../../../src/utils/axiosRequest";
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
 interface CollectionProps {
   id: string;
 }
 
 export default function Collection(props: CollectionProps) {
-  // useEffect(() => {
+  const [collection, setCollection] = useState([]);
+  const fetchImages = async () => {
+    try {
+      const res = await axiosRequest(
+        `/api/user-collect/${props.id}`,
+        "GET"
+      );
+      if (res.status === 200) {
+        console.log(res.data)
+        //setCollection([...res.data]);
+        
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, []);
 
   return (
     <>
