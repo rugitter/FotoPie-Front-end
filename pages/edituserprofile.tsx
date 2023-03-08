a3ca1da86e845a84115cb6b3e5bddf2b97de818a
Edward Chen <kunhong03@gmail.com>
2023-03-09 00:55:49 +1100

    added nav bar

--- pages/edituserprofile.tsx
+++ pages/edituserprofile.tsx
@@ -30,8 +30,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Schema, string, object, mixed } from "yup";
 import { useRouter } from "next/router";
 import axiosRequest from "../src/utils/axiosRequest";
-import { deepOrange, deepPurple } from "@mui/material/colors";
-import Image from "next/image";
+import NavBar from "../src/components/NavBar";
 
 // Define a type with the shape of the form values
 interface IFormInput {
@@ -114,264 +113,269 @@ export default function EditUserProfile() {
   };
 
   return (
-    <FormProvider {...methods}>
-      <Container component="main" maxWidth="md">
-        {/*Page heading, profile icon and change picture button*/}
-
-        <Box
-          sx={{
-            marginTop: 10,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            marginBottom: 10,
-          }}
-        >
-          <Typography component="h1" variant="h3">
-            Profile Setting
-          </Typography>
-        </Box>
-
-        {/*change avatar button*/}
-        <Grid container spacing={10}>
-          <Grid item xs={2}>
-            <Avatar src={avatar} sx={{ width: 130, height: 130 }}></Avatar>
-          </Grid>
-          <Grid item xs={10}>
-            <Button
-              size="medium"
-              variant="contained"
-              color="secondary"
-              sx={{ mt: 5, mb: 8 }}
-              component="label"
-            >
-              Change Picture
-              <input
-                hidden
-                type="file"
-                accept="image/*"
-                onChange={handleFileUpload}
-              />
-            </Button>
-          </Grid>
-        </Grid>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-          }}
-        >
-          {/*First input grid*/}
-
-          <Grid container spacing={8}>
-            <Grid item xs={6}>
-              <Typography variant="h6">First Name*</Typography>
-              <FormTextField
-                name="firstName"
-                label={FirstName}
-                id="firstName"
-                autoComplete="firstName"
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <Typography variant="h6">Last Name*</Typography>
-              <FormTextField
-                name="lastName"
-                label={LastName}
-                id="lastName"
-                autoComplete="LastName"
-              />
-            </Grid>
-          </Grid>
-          <Typography variant="body1" color="grey">
-            We want people to use their real names in the community so they can
-            get to know each other.
-          </Typography>
-
-          {/*Box containing password reset*/}
-
-          <Box component="form" sx={{ mt: 3 }}>
-            <Typography component="h1" variant="h6">
-              password
-            </Typography>
-
-            <Button
-              type="submit"
-              size="medium"
-              variant="contained"
-              color="secondary"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              <Link href="reset-request" color="inherit" underline="none">
-                Reset Password
-              </Link>
-            </Button>
-          </Box>
-
-          {/*Box for writing bio*/}
+    <>
+      <NavBar isFixed={false} color="#000000" />
+      <FormProvider {...methods}>
+        <Container component="main" maxWidth="md">
+          {/*Page heading, profile icon and change picture button*/}
 
           <Box
             sx={{
-              width: 850,
-              height: 250,
+              marginTop: 10,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginBottom: 10,
             }}
           >
-            <Typography component="h1" variant="h4">
-              Recognition
-            </Typography>
-
-            <TextField
-              sx={{
-                width: 850,
-              }}
-              InputProps={{ sx: { height: 150 } }}
-              name="bio"
-              label="Write a short bio for your profile"
-              id="bio"
-              autoComplete="bio"
-            />
-            <Typography variant="body1" color="grey" align="right">
-              0/130
+            <Typography component="h1" variant="h3">
+              Profile Setting
             </Typography>
           </Box>
 
-          {/*Second input grid*/}
-
-          <Grid container spacing={8}>
-            <Grid item xs={6}>
-              <Typography variant="h6">Location</Typography>
-              <FormTextField
-                name="location"
-                label="location"
-                id="location"
-                autoComplete="location"
-              />
-              <Typography variant="body1" color="grey">
-                By sharing your location information, you help us make your
-                profile more identifiable.
-              </Typography>
+          {/*change avatar button*/}
+          <Grid container spacing={10}>
+            <Grid item xs={2}>
+              <Avatar src={avatar} sx={{ width: 130, height: 130 }}></Avatar>
             </Grid>
-
-            <Grid item xs={6}>
-              <Typography variant="h6">Website</Typography>
-              <FormTextField
-                name="website"
-                label="website"
-                id="website"
-                autoComplete="website"
-              />
-              <Typography variant="body1" color="grey">
-                Your profile page, homepage, blog.
-              </Typography>
-            </Grid>
-
-            <Grid item xs={6}>
-              <Typography variant="h6">Twitter</Typography>
-              <FormTextField
-                name="twitter"
-                label="twitter"
-                id="twitter"
-                autoComplete="twitter"
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <Typography variant="h6">Instagram</Typography>
-              <FormTextField
-                name="instagram"
-                label="instagram"
-                id="instagram"
-                autoComplete="instagram"
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <Typography variant="h6">Youtube</Typography>
-              <FormTextField
-                name="youtube"
-                label="youtube"
-                id="youtube"
-                autoComplete="youtube"
-              />
-            </Grid>
-
-            <Grid item xs={6}>
-              <Typography variant="h6">TikTok</Typography>
-              <FormTextField
-                name="tiktok"
-                label="tiktok"
-                id="tiktok"
-                autoComplete="tiktok"
-              />
+            <Grid item xs={10}>
+              <Button
+                size="medium"
+                variant="contained"
+                color="secondary"
+                sx={{ mt: 5, mb: 8 }}
+                component="label"
+              >
+                Change Picture
+                <input
+                  hidden
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileUpload}
+                />
+              </Button>
             </Grid>
           </Grid>
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "start",
+            }}
+          >
+            {/*First input grid*/}
 
-          <Box>
-            {/*Message notification setting*/}
+            <Grid container spacing={8}>
+              <Grid item xs={6}>
+                <Typography variant="h6">First Name*</Typography>
+                <FormTextField
+                  name="firstName"
+                  label={FirstName}
+                  id="firstName"
+                  autoComplete="firstName"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h6">Last Name*</Typography>
+                <FormTextField
+                  name="lastName"
+                  label={LastName}
+                  id="lastName"
+                  autoComplete="LastName"
+                />
+              </Grid>
+            </Grid>
+            <Typography variant="body1" color="grey">
+              We want people to use their real names in the community so they
+              can get to know each other.
+            </Typography>
+
+            {/*Box containing password reset*/}
+
+            <Box component="form" sx={{ mt: 3 }}>
+              <Typography component="h1" variant="h6">
+                password
+              </Typography>
+
+              <Button
+                type="submit"
+                size="medium"
+                variant="contained"
+                color="secondary"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                <Link href="reset-request" color="inherit" underline="none">
+                  Reset Password
+                </Link>
+              </Button>
+            </Box>
+
+            {/*Box for writing bio*/}
+
             <Box
               sx={{
-                marginTop: 8,
                 width: 850,
-                height: 150,
+                height: 250,
               }}
             >
               <Typography component="h1" variant="h4">
-                Message notification
+                Recognition
               </Typography>
 
-              <Typography component="h1" variant="h6">
-                Fotopie moderates all messages to prevent spam.
-              </Typography>
-
-              <FormControlLabel
-                control={<Checkbox value="showMessageButton" color="primary" />}
-                label="Show message button on my profile"
+              <TextField
+                sx={{
+                  width: 850,
+                }}
+                InputProps={{ sx: { height: 150 } }}
+                name="bio"
+                label="Write a short bio for your profile"
+                id="bio"
+                autoComplete="bio"
               />
-            </Box>
-
-            {/*Other setting*/}
-            <Box
-              sx={{
-                marginTop: 8,
-                width: 850,
-                height: 150,
-              }}
-            >
-              <Typography variant="h4">Other settings</Typography>
-
-              <Typography variant="h6" color="grey">
-                Delete account and all data
+              <Typography variant="body1" color="grey" align="right">
+                0/130
               </Typography>
-
-              <Link href="delete account" variant="h6">
-                delete account
-              </Link>
             </Box>
 
-            {/*change name button*/}
-            <Grid
-              container
-              justifyContent="center"
-              component="form"
-              onSubmit={methods.handleSubmit(onSubmit)}
-            >
-              <Button
-                type="submit"
-                size="large"
-                color="secondary"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Save Profile
-              </Button>
+            {/*Second input grid*/}
+
+            <Grid container spacing={8}>
+              <Grid item xs={6}>
+                <Typography variant="h6">Location</Typography>
+                <FormTextField
+                  name="location"
+                  label="location"
+                  id="location"
+                  autoComplete="location"
+                />
+                <Typography variant="body1" color="grey">
+                  By sharing your location information, you help us make your
+                  profile more identifiable.
+                </Typography>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h6">Website</Typography>
+                <FormTextField
+                  name="website"
+                  label="website"
+                  id="website"
+                  autoComplete="website"
+                />
+                <Typography variant="body1" color="grey">
+                  Your profile page, homepage, blog.
+                </Typography>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h6">Twitter</Typography>
+                <FormTextField
+                  name="twitter"
+                  label="twitter"
+                  id="twitter"
+                  autoComplete="twitter"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h6">Instagram</Typography>
+                <FormTextField
+                  name="instagram"
+                  label="instagram"
+                  id="instagram"
+                  autoComplete="instagram"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h6">Youtube</Typography>
+                <FormTextField
+                  name="youtube"
+                  label="youtube"
+                  id="youtube"
+                  autoComplete="youtube"
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h6">TikTok</Typography>
+                <FormTextField
+                  name="tiktok"
+                  label="tiktok"
+                  id="tiktok"
+                  autoComplete="tiktok"
+                />
+              </Grid>
             </Grid>
+
+            <Box>
+              {/*Message notification setting*/}
+              <Box
+                sx={{
+                  marginTop: 8,
+                  width: 850,
+                  height: 150,
+                }}
+              >
+                <Typography component="h1" variant="h4">
+                  Message notification
+                </Typography>
+
+                <Typography component="h1" variant="h6">
+                  Fotopie moderates all messages to prevent spam.
+                </Typography>
+
+                <FormControlLabel
+                  control={
+                    <Checkbox value="showMessageButton" color="primary" />
+                  }
+                  label="Show message button on my profile"
+                />
+              </Box>
+
+              {/*Other setting*/}
+              <Box
+                sx={{
+                  marginTop: 8,
+                  width: 850,
+                  height: 150,
+                }}
+              >
+                <Typography variant="h4">Other settings</Typography>
+
+                <Typography variant="h6" color="grey">
+                  Delete account and all data
+                </Typography>
+
+                <Link href="delete account" variant="h6">
+                  delete account
+                </Link>
+              </Box>
+
+              {/*change name button*/}
+              <Grid
+                container
+                justifyContent="center"
+                component="form"
+                onSubmit={methods.handleSubmit(onSubmit)}
+              >
+                <Button
+                  type="submit"
+                  size="large"
+                  color="secondary"
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Save Profile
+                </Button>
+              </Grid>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </FormProvider>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </FormProvider>
+    </>
   );
 }
