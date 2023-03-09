ce0e6650f6830256e7603a57f3b68f203777f4e4
evan0626ma <yuchen0626ma@gmail.com>
2023-03-09 23:13:37 +1100

    feat:user profile gallery post

--- src/components/profile/gallery.tsx
+++ src/components/profile/gallery.tsx
@@ -9,37 +9,42 @@ interface GalleryProps {
   id: string;
 }
 
+interface ResponseImageData {
+  _id: string,
+  price: number,
+  tag: string,
+  userEmail: string,
+  imageUrl: string,
+}
 
 export default function Gallery(props: GalleryProps) {
 
-  const [galleryPosts, setGalleryPosts] = useState("");
+  const [galleryPosts, setGalleryPosts] = useState([]);
   const router = useRouter();
   const { id } = router.query;
 
   useEffect(() => {
     if (!router.isReady) return;
-
     axiosRequest(`/api/profile/${id}`, "GET").then((res) => {
-      console.log(res.data);
-      
-  
+      const image = res.data.map((image: ResponseImageData) => (image.imageUrl
+      ))
+      console.log(image);
+      setGalleryPosts(image);
+
     });
   }, [id]);
   return (
     <>
-      {/* <h1>Gallery</h1>
-      <h2>{props.id}</h2> */}
-
       <Box sx={{ width: "100%", height: "100%", overflowY: "scroll" }}>
         <ImageList
           sx={{ columnCount: { sm: `2 !important`, md: `3 !important` } }}
           variant="masonry"
           gap={8}
         >
-          {itemData.map((item) => (
-            <ImageListItem key={item.img}>
+          {galleryPosts.map((imageUrl, i) => (
+            <ImageListItem key={i} >
               <img
-                src={`${item.img}?w=500`}
+                src={`${imageUrl}?w=500`}
                 loading="lazy"
               />
             </ImageListItem>
@@ -47,57 +52,5 @@ export default function Gallery(props: GalleryProps) {
         </ImageList>
       </Box>
     </>
-
   );
 }
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-    title: 'Bed',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-    title: 'Books',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-    title: 'Sink',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-    title: 'Kitchen',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-    title: 'Blinds',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-    title: 'Chairs',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-    title: 'Laptop',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-    title: 'Doors',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-    title: 'Storage',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-    title: 'Candle',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-    title: 'Coffee table',
-  },
-];
