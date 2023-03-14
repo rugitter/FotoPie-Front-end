e379b73ffb16950e2888c2a2505409c7664f11ac
Edward Chen <kunhong03@gmail.com>
2023-03-15 03:03:40 +1100

    Update: added infinite scrolling on collection posts

--- src/components/profile/Collection.tsx
+++ src/components/profile/Collection.tsx
@@ -5,6 +5,11 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import Post from "../PostList/Post";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "../Loader/Loader";
+import Masonry from "@mui/lab/Masonry";
+import NoMore from "../Loader/NoMore";
 
 interface CollectionProps {
   id: string;
@@ -18,23 +23,28 @@ interface ResponseImageData {
 }
 
 export default function Collection(props: CollectionProps) {
-  const [collection, setCollection] = useState([]);
+  const [collection, setCollection] = useState<ResponseImageData[]>([]);
+  const [page, setPage] = useState(1);
+  const [loaderHandler, setLoaderHandler] = useState(true);
+
+  const [Error, setError] = useState(null);
+
+  let limit = 10;
   const fetchImages = async () => {
     try {
       const res = await axiosRequest(
-        `/api/user-collect/${props.id}`,
+        `/api/user-collect/${props.id}?page=${page}&limit=${limit}`,
         "GET"
       );
       if (res.status === 200) {
-        console.log(res.data)
-        const imageUrlArray = res.data.map((image: ResponseImageData) => image.imageUrl);
-        console.log(imageUrlArray)
-        setCollection(imageUrlArray);
-        console.log(collection)
-        
+        setCollection([...collection, ...res.data]);
+        setPage(page + 1);
+        if ([...res.data].length === 0) {
+          setLoaderHandler(false);
+        }
       }
     } catch (error: any) {
-      console.log(error.message);
+      setError(error.message);
     }
   };
   useEffect(() => {
@@ -45,7 +55,7 @@ export default function Collection(props: CollectionProps) {
     <>
       <h1>Collection</h1>
       {/*<h2>{props.id}</h2>*/}
-      <ImageList sx={{ width: 1000, height: 900 }} cols={3} rowHeight={328}>
+      {/*<ImageList sx={{ width: 1000, height: 900 }} cols={3} rowHeight={328}>
         {collection.map((item) => (
           <ImageListItem key={item}>
             <img
@@ -56,28 +66,25 @@ export default function Collection(props: CollectionProps) {
             />
           </ImageListItem>
         ))}
-      </ImageList>
-      {/*<Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 1, sm: 0.5, md: 0.5 }}
-          columns={{ xs: 12, sm: 12, md: 12 }}
-          columnSpacing={{ xs: 1, sm: 1, md: 0.5 }}
+      </ImageList>*/}
+      <Box sx={{ width: "100%", height: "100%", overflowY: "scroll" }}>
+        <InfiniteScroll
+          dataLength={collection.length}
+          next={fetchImages}
+          hasMore={true}
+          loader={loaderHandler ? <Loader /> : <NoMore />}
         >
-          {collection.map((image,index) => (
-            <Grid item key={image.id} xs={12} sm={6} md={4}>
-              <img
-                src={image}
-                style={{
-                  height: "300px",
-                  width: "300px",
-                  objectFit: 'cover'
-                }}
+          <Masonry columns={{ sm: 2, md: 3 }} spacing={2} sx={{ m: "auto" }}>
+            {collection.map((collection) => (
+              <Post
+                url={collection.imageUrl}
+                filename={collection.imageUrl}
+                key={collection._id}
               />
-            </Grid>
-          ))}
-        </Grid>
-              </Box>*/}
+            ))}
+          </Masonry>
+        </InfiniteScroll>
+      </Box>
     </>
   );
 }
