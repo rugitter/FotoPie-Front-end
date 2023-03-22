b04b7ce81638790635a75e99f37643578c45e507
Yao Wu <bunny.yaowu@gmail.com>
2023-03-23 00:40:59 +1100

    fix

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -72,6 +72,33 @@ export default function Navbar({
     };
   }, [isFixed]);
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const mobileMenuId = "primary-search-account-menu-mobile";
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -203,7 +230,7 @@ export default function Navbar({
             <IconButton
               size="large"
               aria-haspopup="true"
-              // onClick={handleMobileMenuOpen}
+              onClick={handleMobileMenuOpen}
               color="inherit"
             >
               <MenuIcon sx={{ color: fix ? "black" : color }} />
