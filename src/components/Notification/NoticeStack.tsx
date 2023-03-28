62b21410221948e0f855ea3b879641e43b8204a4
Adamyyyy <happyadam2008@gmail.com>
2023-03-28 20:45:53 +1100

    fix: use useCallback hook to call notification fetchData

---
+++ src/components/Notification/NoticeStack.tsx
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchNotifications } from "../../../store/notification/notifyAction";
+import { AppDispatch, RootState } from "../../../store/store";
+import Loading from "./Loading";
+import NoNotification from "./NoNotification";
+import NotificationList from "./NotificationList";
+
+export default function NoticeStack() {
+  //RTK Dispatch and fetch data
+  const dispatch = useDispatch<AppDispatch>();
+  const { notifications, status } = useSelector(
+    (state: RootState) => state.notifySlice
+  );
+
+  const fetchData = useCallback(() => {
+    dispatch(fetchNotifications());
+  }, [dispatch]);
+
+  //fetch data
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  //render Loading/no notification tips/notification list
+  if (status === "loading" || status === "idle") {
+    return <Loading />;
+  } else if (notifications.length === 0) {
+    return <NoNotification />;
+  } else {
+    return <NotificationList notifications={notifications} />;
+  }
+}
