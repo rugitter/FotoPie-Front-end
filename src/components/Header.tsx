9a875f8db1850d6bb7090431874e422ee6916458
Yingxin cai <cai19980122@gmail.com>
2023-03-29 17:04:20 +1100

    s

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,8 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import SearchBar from './Search/SearchBar';
+import Link from '@mui/material/Link';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 
 export default function Header() {
@@ -21,10 +23,11 @@ export default function Header() {
           mt: 8,
           ml: 'auto',
           mr: 'auto',
+          // height:300,
           display: 'flex',
-          justifyContent: 'center',
+          justifyContent: 'flex-end',
           flexDirection: 'column',
-          alignItems: 'flex-start',
+          alignContent: 'center',
         }}
       >
         <Typography
@@ -34,25 +37,71 @@ export default function Header() {
           color='#FFFFFF'
           sx={{ fontSize: 22, fontFamily: 'inherit', textAlign: 'left' }}
         >
-          Start shining.<br />
-          Start earning.<br />
-          With your beloved shots.
+          Discover moments.
+          <br />
+          Find inspiration.
         </Typography>
 
         <Paper
           sx={{
             p: '2px 4px',
             width: '100%',
-            mb: 15,
+            // mb: 15,
             mt: 2,
             '@media (min-width: 600px)': {
               width: 500,
             }
           }}
         >
-          <SearchBar sx={{ width: '100%' }}/>
-        </Paper>
+          <SearchBar sx={{ width: '100%' }} />
+       
+        </Paper> 
+
+ 
+        <Typography
+          variant="subtitle2"
+          align="center"
+          display="inline"
+          color="#FFFFFF"
+          sx={{
+            fontSize: 14,
+            fontFamily: 'inherit',
+            textAlign: 'left',
+            mt: 1,
+            mb: 15,
+            lineHeight: 1.5,
+            '& a': { textDecoration: 'none', mx: 0.5 },
+          }}
+        >
+          <span style={{ fontSize: 18, opacity:0.7}}>Trending:</span>
+          <Box component="span" sx={{ ml: 0.5 }}>
+            <Link href="/category/flowers" color="inherit">
+              Food,
+            </Link>
+            <Link href="#" color="inherit">
+              Cars,
+            </Link>
+            <Link href="#" color="inherit">
+              Building,
+            </Link>
+            <Link href="#" color="inherit">
+              City,
+            </Link>
+            <Link href="#" color="inherit">
+              Nature
+            </Link>
+          </Box>
+          <Box display="inline-flex" alignItems="center" sx={{ marginLeft: '2px'}}>
+            <Link href="/category/category-main-page" color="inherit">
+              <MoreHorizIcon />
+            </Link>
+          </Box>
+        </Typography>
+      
+
       </Box>
+      
+    
     </Stack>
   );
 }
