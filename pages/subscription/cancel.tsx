44c79556e55d4413ef6c1b52b543f1685684107c
Jeremy Liu <unswmercury@gmail.com>
2023-04-05 12:22:06 +1000

    Add payment cancelled page

--- pages/subscription/cancel.tsx
+++ pages/subscription/cancel.tsx
@@ -1,10 +1,44 @@
 import Copyright from "../../src/components/Copyright";
+import { NavBarStyles } from "../../src/components/NavBar/NavbarBaseline.style";
+import NavBar from "../../src/components/NavBar/NavBar";
+import Button from "@mui/material/Button";
+import Link from "next/link";
+import Box from "@mui/material/Box";
+import Image from "mui-image";
 
 export default function SubscriptionPage() {
   return (
     <>
-      <h1>Payment Cancelled</h1>
+      <NavBar isFixed={false} color="#000000" baseLine={NavBarStyles} />
 
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{ width: "30%" }}>
+          <Image src="/payment-cancelled.jpg" alt=""></Image>
+        </Box>
+        <h1>Payment Cancelled</h1>
+        <br />
+        <Link href="/" passHref style={{ textDecoration: "none" }}>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "primary.main",
+              color: "white",
+              "&:hover": {
+                backgroundColor: "primary.dark",
+              },
+            }}
+          >
+            Home Page
+          </Button>
+        </Link>
+      </Box>
       <Copyright />
     </>
   );
